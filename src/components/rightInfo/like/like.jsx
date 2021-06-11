59d7eed9b0bc5e24b8655a50430c01b556d515ee
dewei zhang <377805163@qq.com>
2021-06-11 23:07:43 +0800

    like css

--- src/components/rightInfo/like/like.jsx
+++ src/components/rightInfo/like/like.jsx
@@ -3,7 +3,17 @@ import './like.css'
 
 const Like=()=>{
 
-    return <div className="heart"></div>
+    return (<div className="heart">
+                <div id="outer">
+                    <div id="div1">
+                    </div>
+                    <div id="div2">
+                    </div>
+                    <div id="div3">
+                    </div>
+                </div>
+
+    </div>)
 }
 
 export default Like;
